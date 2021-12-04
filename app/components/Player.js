f67a25f3b211ef78e22f66c482d4f319bfd18235
Ram Singh <mndeep204@gmail.com>
2021-12-04 16:36:29 +0530

    player fixed

--- app/components/Player.js
+++ app/components/Player.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Dimensions, StyleSheet} from 'react-native';
+import {View, Text, Dimensions, StyleSheet, Image} from 'react-native';
 import {scale} from 'react-native-size-matters';
 import Carousel from 'react-native-snap-carousel';
 import Container from './Container';
@@ -12,12 +12,16 @@ export default function Player() {
   const _renderItem = ({item, index}) => {
     return (
       <View style={styles.itemWrap}>
-          <View
-          style={{backgroundColor: 'green', height: scale(200), width: '80%'}}>
-          <Text>{item}</Text>
-        </View>  
+        <Image
+          style={{
+            borderRadius: scale(15),
+            height: scale(300),
+            width: SCREEEN.width - 70,
+          }}
+          source={{uri: 'https://i.ytimg.com/vi/H-dz24i33OY/maxresdefault.jpg'}}
+        />
 
-        <View style={{paddingHorizontal: scale(20)}}>{_activeSong()}</View>
+        <View style={{paddingVertical: scale(20)}}>{_activeSong()}</View>
       </View>
     );
   };
@@ -25,19 +29,19 @@ export default function Player() {
   const _activeSong = () => {
     return (
       <View style={{justifyContent: 'center', alignItems: 'center'}}>
-        <Label text="Sin" />
-        <Label text="Sidhu moosewala" />
+        <Label text="Alvida" style={{fontSize: scale(22)}} />
+        <Label text="Raj Ranjodh" style={{color: appColors.lightWhite}} />
       </View>
     );
   };
   const _renderAction = () => {
     return (
       <View style={styles.actionCon}>
-        <MaterialIcons name="add" size={scale(30)} color={appColors.white} />
-        <MaterialIcons name="image" size={scale(30)} color={appColors.white} />
+        <MaterialIcons name="add" size={scale(25)} color={appColors.white} />
+        <MaterialIcons name="image" size={scale(25)} color={appColors.white} />
         <MaterialIcons
           name="more-vert"
-          size={scale(30)}
+          size={scale(25)}
           color={appColors.white}
         />
       </View>
@@ -56,28 +60,46 @@ export default function Player() {
       </>
     );
   };
-  const _renderBelowActions = ()=>{
-      return <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-           <MaterialIcons name="report" size={scale(40)} color={appColors.white} />
-           <MaterialIcons name="skip-previous" size={scale(40)} color={appColors.white} />
-           <MaterialIcons name="pause" size={scale(40)} color={appColors.white} />
-           <MaterialIcons name="skip-next" size={scale(40)} color={appColors.white} />
-           <MaterialIcons name="favorite-border" size={scale(40)} color={appColors.white} />
+  const _renderBelowActions = () => {
+    return (
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <MaterialIcons name="report" size={scale(35)} color={appColors.white} />
+        <MaterialIcons
+          name="skip-previous"
+          size={scale(35)}
+          color={appColors.white}
+        />
+        <MaterialIcons name="pause" size={scale(35)} color={appColors.white} />
+        <MaterialIcons
+          name="skip-next"
+          size={scale(35)}
+          color={appColors.white}
+        />
+        <MaterialIcons
+          name="favorite-border"
+          size={scale(35)}
+          color={appColors.white}
+        />
       </View>
-  }
+    );
+  };
   return (
     <Container>
-      
-      <Carousel
-        layout={'default'}
-        data={[1, 2, 3]}
-         renderItem={_renderItem}
-        sliderWidth={SCREEEN.width}
-        itemWidth={SCREEEN.width}
-      />
-      {_renderAction()}
-      {_renderTrack()}
-      {_renderBelowActions()}
+      <View style={{height: scale(400)}}>
+        <Carousel
+          layout={'default'}
+          data={[1, 2, 3]}
+          renderItem={_renderItem}
+          sliderWidth={SCREEEN.width}
+          itemWidth={SCREEEN.width}
+            
+        />
+      </View>
+      <View style={{paddingHorizontal: scale(20)}}>
+        {_renderAction()}
+        {_renderTrack()}
+        {_renderBelowActions()}
+      </View>
     </Container>
   );
 }
@@ -94,15 +116,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingVertical: scale(20),
+    borderRadius: scale(10),
+    overflow: 'hidden',
   },
   seekCon: {
-    backgroundColor: 'red',
-    height: scale(5),
+    backgroundColor: appColors.lightWhite,
+    height: scale(4),
     borderRadius: scale(5),
   },
   seekInnerCon: {
-    backgroundColor: 'green',
-    height: scale(5),
+    backgroundColor: appColors.sky,
+    height: scale(4),
     width: '50%',
     borderRadius: scale(5),
   },
